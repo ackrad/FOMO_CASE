f2d5d26a74ecb1c1ed195bc45a221e43a52416e4
Ege Çorakay <80698720+ackrad@users.noreply.github.com>
2024-12-04 19:21:12 +0300

    start work on gridmanager

---
+++ Assets/GridManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private Vector2Int gridSize;
+    [SerializeField] private float cellSize =1f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
